9de48eb10fe735c3e7269cf70f498970a195ede1
Aetos <nicolas.torres-sannier@proton.me>
2023-06-26 23:09:33 +0200

    Update Client.c

--- Client/Client.c
+++ Client/Client.c
@@ -1,9 +1,9 @@
-#include "FonctionsClient.h"
+#include "ClientFunctions.h"
 
 int main(int argc, char *argv[]) {
   // argv[1] = 127.0.0.1
   // argv[2] = 8000
 
-  lancer(argv[1], atoi(argv[2]));
+  launch(argv[1], atoi(argv[2]));
   return 0;
 }
